ca061e78d35c146dea8b6bd886d21929137ae84a
Anderson <Anderson@MANIGOLD-PC>
2013-04-24 05:43:46 -0300

    Atualização 24/04/2013 às 04:25 h

--- SICHROLAB/src/java/beans/DisciplinaMB.java
+++ SICHROLAB/src/java/beans/DisciplinaMB.java
@@ -30,6 +30,7 @@ public class DisciplinaMB {
     private List<Disciplina> disciplinas = new ArrayList<Disciplina>();
     
     private String nome_disciplina;
+    private String curso_disciplina;
     private String disciplinaPesquisada;
     private String mensagem;
     
@@ -89,7 +90,17 @@ public class DisciplinaMB {
         }
         setNome_disciplina("");
     }
-
+    
+    public void pesquisarDisciplinaPorCurso(){
+        disciplinas = new ArrayList<Disciplina>();
+        for(Disciplina s : disciplinaDAO.findDisciplinaEntities()){
+            if(s.getCurso().toUpperCase().contains(getCurso_disciplina().toUpperCase())){
+                disciplinas.add(s);
+            }
+        }
+        setNome_disciplina("");
+    }
+    
     /**
      * @return the disciplina
      */
@@ -158,6 +169,20 @@ public class DisciplinaMB {
      */
     public void setNome_disciplina(String nome_disciplina) {
         this.nome_disciplina = nome_disciplina;
+    }
+
+    /**
+     * @return the curso_disciplina
+     */
+    public String getCurso_disciplina() {
+        return curso_disciplina;
+    }
+
+    /**
+     * @param curso_disciplina the curso_disciplina to set
+     */
+    public void setCurso_disciplina(String curso_disciplina) {
+        this.curso_disciplina = curso_disciplina;
     }
     
 }
